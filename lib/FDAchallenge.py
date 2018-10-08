83650c9a1f536971b31f0446f72fcadd48e8655f
Marouen <marouen.b.guebila@gmail.com>
2018-10-08 14:57:45 +0100

    trying xgboost

--- lib/FDAchallenge.py
+++ lib/FDAchallenge.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier #call extra trees
-from sklearn.linear_model import RandomizedLasso #feature selection
 import random #to set the seed
 import os
 from sklearn.model_selection import cross_val_score #cross validation
+from sklearn.feature_selection import SelectFromModel #select top features
+from xgboost import XGBClassifier #for xgboost
 
 #change working directory
 os.chdir('/home/marouen/challenges/FDA/data')
@@ -17,8 +18,8 @@ labels = pd.read_csv("train_cli.tsv", sep="\t")
 matching = pd.read_csv("sum_tab_1.csv")
 
 #Mesure size of classes
-print labels.groupby(['gender']).size()
-print labels.groupby(['msi']).size()
+print(labels.groupby(['gender']).size())
+print(labels.groupby(['msi']).size())
 
 #Encode sex and MSI
 labels['gender'].replace(['Female','Male'],[1,0], inplace=True)
@@ -32,9 +33,12 @@ sum(matching["mismatch"])
 #for now impute by mean per row (patient) 
 #as mean by column (protein) yields entire NAN columns -> worth thr try anyway as feature selection
 training=training.T.fillna(training.mean(axis=1)).T
-#add sex and disease as features
-training.loc[:,'gender']=labels.iloc[:,1].values
-training.loc[:,'msi']=labels.iloc[:,2].values
+
+#remove mismatches for now
+indices = np.where(matching["mismatch"]==0)[0]
+misMatcInd = np.where(matching["mismatch"]==1)[0]
+trainingNoMismatch = training.iloc[indices,:]
+labelsNoMismatch = labels.iloc[indices,:]
 
 #More exploration for data imputation
 #distribution of proteins by sex
@@ -45,54 +49,31 @@ training.loc[:,'msi']=labels.iloc[:,2].values
 #Optimize hyperparameters
 
 #feature selection though stability selection
-nFeatures=20
-rlasso = RandomizedLasso(alpha=0.01, sample_fraction=0.75, n_resampling=3000)#need high resampling to
-rlasso.fit(training, matching.iloc[:,1])
-names=list(training)
+trainingNoMismatch.loc[:,'add']=labelsNoMismatch.iloc[:,2].values
+#random.seed(1)
+classToPredict=1 #could be 1 (sex) or 2 (msi)
+clf = ExtraTreesClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0, class_weight="balanced")  #need high resampling to
+clf.fit(trainingNoMismatch, labelsNoMismatch.iloc[:,classToPredict]) #predict sex 1 disease 2
+names=list(trainingNoMismatch)
 #print "Features sorted by their score:"
 #print sorted(zip(map(lambda x: round(x, 4), rlasso.scores_), names), reverse=True)
-vecSort=sorted(zip(map(lambda x: round(x, 4), rlasso.scores_), names), reverse=True)
+vecSort=sorted(zip(map(lambda x: round(x, 4), clf.feature_importances_), names), reverse=True)
 colNameVecSort=[x[1] for x in vecSort]
-selFeatures=colNameVecSort[0:nFeatures]#select 30 features
-print vecSort[0]
+#print(colNameVecSort)
 
-#learn and cross validate : target is gender
-clf = ExtraTreesClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0, class_weight="balanced") 
-clf.fit(training.loc[:,selFeatures], matching)
-scores = cross_val_score(clf, training.loc[:,selFeatures], matching.iloc[:,1], cv=5)
-print scores.mean()
+scoreVec=[]
+featVec=[1,2,3,4,5,6,7,8,9,10,15,16,17,18,19,20,30,40,50,60,70,80,90,100]
+for nFeatures in featVec:
+	print(nFeatures)
+	selFeatures=colNameVecSort[0:nFeatures] #select nFeatures
+	#print(selFeatures)
+	scores = cross_val_score(clf, trainingNoMismatch.loc[:,selFeatures], labelsNoMismatch.iloc[:,classToPredict], cv=5, scoring='balanced_accuracy')
+	scoreVec.append(scores.mean())
 
+print(list(zip(featVec,scoreVec)))
+
+
+xgboot = XGBClassifier()
+#y_pred = model.predict(X_test)
 #y=clf.predict(training.iloc[misMatcInd,:].loc[:,selFeatures])   
 #y==labels.iloc[misMatcInd,classToPredict]
-
-#disease
-'''
-1      True
-9     False
-12     True
-38    False
-46     True
-57    False
-64     True
-67    False
-71    False
-77    False
-78     True
-79     True
-'''
-
-#sex
-'''
-1     False
-9      True
-12     True
-38    False
-46     True
-57     True
-64    False
-67    False
-71     True
-77     True
-78    False
-79    False
-'''
