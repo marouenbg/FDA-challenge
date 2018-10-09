ebbd4235aa11e756a74668a17ffd75b1a47e446f
Marouen <marouen.b.guebila@gmail.com>
2018-10-09 13:50:24 +0100

    getting 1 everywhere need to fix

--- lib/FDAchallenge.py
+++ lib/FDAchallenge.py
@@ -23,16 +23,16 @@ labels = pd.read_csv("train_cli.tsv", sep="\t")
 matching = pd.read_csv("sum_tab_1.csv")
 
 #Mesure size of classes
-print(labels.groupby(['gender']).size())
-print(labels.groupby(['msi']).size())
+#print(labels.groupby(['gender']).size())
+#print(labels.groupby(['msi']).size())
 
 #Encode sex and MSI
 labels['gender'].replace(['Female','Male'],[0,1], inplace=True)
 labels['msi'].replace(['MSI-Low/MSS','MSI-High'],[0,1], inplace=True)
-print(sum(labels[labels['gender'] == 1]['msi'])/sum(labels['gender'] == 1))
-print(sum(labels[labels['gender'] == 0]['msi'])/sum(labels['gender'] == 0))
-print(sum(labels[labels['msi'] == 1]['gender'])/sum(labels['msi'] == 1))
-print(sum(labels[labels['msi'] == 0]['gender'])/sum(labels['msi'] == 0))
+#print(sum(labels[labels['gender'] == 1]['msi'])/sum(labels['gender'] == 1))
+#print(sum(labels[labels['gender'] == 0]['msi'])/sum(labels['gender'] == 0))
+#print(sum(labels[labels['msi'] == 1]['gender'])/sum(labels['msi'] == 1))
+#print(sum(labels[labels['msi'] == 0]['gender'])/sum(labels['msi'] == 0))
 
 #data exploration
 training.max().plot(kind='hist')
@@ -48,18 +48,29 @@ if impute=='mean':
 	#training=(training-training.mean())/training.std()
 else:
 	#identify x and y proteins
+	indices=np.where(matching["mismatch"]==0)[0]
 	r=re.compile(".+(X|Y)")
-	prot = list(filter(r.match,list(training)))
-	maleInd=np.where(labels["gender"]==1)[0]
-	femaleInd=np.where(labels["gender"]==0)[0]
-	meanMale=np.mean(training.iloc[maleInd,:].loc[:,prot])
+	prot      =list(filter(r.match,list(training))) #matching XY proteins
+	msiProt   =list(set(list(training)) - set(prot)) #non matching prot
+	maleInd   =list( set(np.where(labels["gender"]==1)[0]) & set(indices))
+	femaleInd =list( set(np.where(labels["gender"]==0)[0]) & set(indices))
+	msiLoInd  =list( set(np.where(labels["msi"]==1)[0]) & set(indices))
+	msiHiInd  =list( set(np.where(labels["msi"]==0)[0]) & set(indices))
+	meanMale  =np.mean(training.iloc[maleInd,:].loc[:,prot])
 	meanFemale=np.mean(training.iloc[femaleInd,:].loc[:,prot])
+	meanMsiLo =np.mean(training.iloc[msiLoInd,:].loc[:,msiProt])
+	meanMsiHi =np.mean(training.iloc[msiHiInd,:].loc[:,msiProt])
 	for i in range(len(prot)):
 		training.loc[:,prot[i]].iloc[maleInd]=training.loc[:,prot[i]].iloc[maleInd].fillna(meanMale[i])
 		training.loc[:,prot[i]].iloc[femaleInd]=training.loc[:,prot[i]].iloc[femaleInd].fillna(meanFemale[i])
 	#Impute the other missing by the mean (consider MSI and further refien prot liek CYP and COX)
+	for i in range(len(msiProt)):
+		training.loc[:,msiProt[i]].iloc[msiLoInd]=training.loc[:,msiProt[i]].iloc[msiLoInd].fillna(meanMsiLo[i])
+		training.loc[:,msiProt[i]].iloc[msiHiInd]=training.loc[:,msiProt[i]].iloc[msiHiInd].fillna(meanMsiHi[i])
+	#Although we can't fill all proteins because some of the proteins in match are completely
+	#na so fill remaining (incl Mismatch) by mean of patient
 	training=training.T.fillna(training.mean(axis=1)).T
-
+	#BONUS: do all of the above in pandas
 
 #remove mismatches for now
 indices = np.where(matching["mismatch"]==0)[0]
@@ -91,7 +102,7 @@ for anteClass in [2,1]: #2 is predicting sex,1 is predicting msi
 	#print(trainingNoMismatch.loc[:,'add1'])
 	#random.seed(1)
 	#classToPredict could be 1 (sex) or 2 (msi)
-	clf = ExtraTreesClassifier(n_estimators=100, class_weight="balanced")  #need high resampling to
+	clf = ExtraTreesClassifier(n_estimators=300, class_weight="balanced")  #need high resampling to
 	clf.fit(trainingNoMismatch, labelsNoMismatch.iloc[:,classToPredict]) #predict sex 1 disease 2
 	names=list(trainingNoMismatch)
 	#print "Features sorted by their score:"
@@ -102,12 +113,13 @@ for anteClass in [2,1]: #2 is predicting sex,1 is predicting msi
 
 	scoreVec=[]
 	featVec=[1,2,3,4,5,6,7,8,9,10,15,16,17,18,19,20,30,40,50,60,70,80,90,100]
-	X_train, X_test, y_train, y_test = train_test_split(trainingNoMismatch, labelsNoMismatch.iloc[:,classToPredict], test_size=0.1, random_state=140)#42
+	#X_train, X_test, y_train, y_test = train_test_split(trainingNoMismatch, labelsNoMismatch.iloc[:,classToPredict], test_size=0.1, random_state=42)#42
 	for nFeatures in featVec:
 		#print(nFeatures)
 		selFeatures=colNameVecSort[0:nFeatures] #select nFeatures
 		#print(selFeatures)
-		scores = cross_val_score(clf, X_train.loc[:,selFeatures], y_train, cv=5, scoring='f1')
+		scores = cross_val_score(clf, trainingNoMismatch.loc[:,selFeatures], labelsNoMismatch.iloc[:,classToPredict], cv=5, scoring='balanced_accuracy')
+		#scores = cross_val_score(clf, X_train.loc[:,selFeatures], y_train, cv=5, scoring='f1')
 		scoreVec.append(scores.mean())
 
 	print(list(zip(featVec,scoreVec)))
@@ -118,19 +130,19 @@ for anteClass in [2,1]: #2 is predicting sex,1 is predicting msi
 	selFeatures=colNameVecSort[0:nFeat]
 	print(selFeatures)
 	xgboost = XGBClassifier(scale_pos_weight=sumneg/sumpos)	
-	#xgboost.fit(trainingNoMismatch.loc[:,selFeatures], labelsNoMismatch.iloc[:,classToPredict])
-	xgboost.fit(X_train, y_train)
-	y_pred=xgboost.predict(X_test)
-	a=accuracy_score(y_test,y_pred)
-	print(a)
+	xgboost.fit(trainingNoMismatch.loc[:,selFeatures], labelsNoMismatch.iloc[:,classToPredict])
+	#xgboost.fit(X_train, y_train)
+	#y_pred=xgboost.predict(X_test)
+	#a=accuracy_score(y_test,y_pred)
+	#print(a)
 	#predict
-	#X_test = training.iloc[misMatchInd,:].loc[:,selFeatures]   
-	#y_pred = xgboost.predict(X_test)
+	X_test = training.iloc[misMatchInd,:].loc[:,selFeatures]   
+	y_pred = xgboost.predict(X_test)
 	#test
 	#y_pred==labels.iloc[misMatchInd,classToPredict]
-	#compMat[:,classToPredict-1]=y_pred
+	compMat[:,classToPredict-1]=y_pred
 
-#print(compMat)
+print(compMat)
 
 #TO dO:
 #How to combine binary and continous features
